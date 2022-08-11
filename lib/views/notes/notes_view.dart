0290aa2ac8a3d0401c14113daa55be9dbfb2e806
Prateek Krishna <prateekkrishna09@gmail.com>
2022-08-11 18:33:11 +0530

    step-13 added new note view

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/enums/menu_action.dart';
 import 'package:mynotes/services/auth/auth_services.dart';
 import 'package:mynotes/services/crud/notes.services.dart';
-
-import '../constants/routes.dart';
+import '../../constants/routes.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({Key? key}) : super(key: key);
@@ -34,8 +33,13 @@ class _NotesViewState extends State<NotesView> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Main UI'),
+          title: const Text('Your Notes'),
           actions: [
+            IconButton(
+                onPressed: (() {
+                  Navigator.of(context).pushNamed(newNoteRoute);
+                }),
+                icon: const Icon(Icons.add)),
             PopupMenuButton<MenuAction>(
               onSelected: (value) async {
                 switch (value) {
